aaa0a8afc8690921b411e69bab1b015391cce7b9
Phillip Kruger <phillip.kruger@gmail.com>
2026-08-03 19:46:17 +1000

    Validate HTTP status code range in HttpProblem.Builder
    Closes #659

--- runtime/src/main/java/io/quarkiverse/httpproblem/HttpProblem.java
+++ runtime/src/main/java/io/quarkiverse/httpproblem/HttpProblem.java
@@ -186,6 +186,9 @@ public class HttpProblem extends RuntimeException {
         }
 
         public Builder withStatus(int statusCode) {
+            if (statusCode < 100 || statusCode > 599) {
+                throw new IllegalArgumentException("HTTP status code must be between 100 and 599, got: " + statusCode);
+            }
             this.statusCode = statusCode;
             return this;
         }
